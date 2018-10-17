f7fb313c83bc9023872037f9d2d3cad3072de990
Ruben de la Fuente <43235289+RubenF1980@users.noreply.github.com>
2018-10-17 13:06:13 +0200

    Add files via upload

---
+++ Punteros/Punteros.h
@@ -0,0 +1,12 @@
+#ifndef PUNTEROS__H
+#define PUNTEROS__H
+
+
+//ejercicio1
+void calcularDiaSiguiente(int *dia, int *mes, int*anio);
+
+//ejercicio2
+
+int difenciaMesesCompletos(int *mes, int *mes2)
+
+#endif
